2c18aaa5673257bd37bdf6427fe8b486e8457ddc
Fabio <fabioferreccio@hotmail.com>
2019-04-07 12:48:25 -0300

    - Mudança de Menu - Adição de classe Windows10, com os dados especificos para tal.

---
+++ ServiceOptimizer/Classes/Windows10.cs
@@ -0,0 +1,251 @@
+﻿using Newtonsoft.Json;
+using ServiceOptimizer.Classes.Modelo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ServiceOptimizer.Classes
+{
+    class Windows10
+    {
+        public Boolean isByte { get; set; }
+
+        public Windows10(ref byte op)
+        {
+            Console.WriteLine("1 - Lists and Backup - Your Windows Services.");
+            Console.WriteLine("2 - BlackViper - Safe for Desktop.");
+            Console.WriteLine("3 - BlackViper - Safe for Laptop or Tablet.");
+            Console.WriteLine("4 - BlackViper - Tweaked for Desktop.");
+            Console.WriteLine("5 - DEFAULT - Windows 10 [HOME].");
+            Console.WriteLine("6 - DEFAULT - Windows 10 [PRO].");
+            Console.WriteLine("7 - Restore client Backup.");
+            Console.WriteLine("0 - Exit.");
+
+            Console.Write($"{Environment.NewLine}Select an option: ");
+            Console.Out.Flush();
+            isByte = Byte.TryParse(Console.ReadLine(), out op);
+        }
+
+        public void loadJSON(ref List<BlackViperModel> listWin10)
+        {
+            string sourceFile = @"ConfigJSON.json";
+            listWin10 = JsonConvert.DeserializeObject<List<BlackViperModel>>(File.ReadAllText(sourceFile));
+        }
+
+        public void loadJSON(ref List<ServiceClientModel> listBackupUser)
+        {
+            string sourceFile = @"ConfigUser.json";
+            listBackupUser = JsonConvert.DeserializeObject<List<ServiceClientModel>>(File.ReadAllText(sourceFile));
+        }
+
+        public void listAndBackup_WindowsServices(ref byte op)
+        {
+            ServicosWin.ListService();
+            op = 255;
+        }
+
+        public void Restore_WindowsServiceBackup(ref byte op, List<ServiceClientModel> listBackupUser)
+        {
+            foreach (ServiceClientModel item in listBackupUser)
+            {
+                string nameService = String.Empty;
+                if (item.Service_Name != null && item.Service_Name.Contains("_"))
+                {
+                    nameService = item.Service_Name.Split('_')[0];
+                }
+                else
+                {
+                    nameService = item.Service_Name;
+                }
+
+                if (item.StartType != null && item.StartType.Contains("Manual"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
+                }
+                else if (item.StartType != null && item.StartType.Contains("Automatic"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
+                }
+                else if (item.StartType != null && item.StartType.Contains("Disabled"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
+                }
+                else
+                {
+                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null" : nameService));
+                }
+            }
+            op = 0;
+        }
+
+        public void setDefault_WindowsHome(ref byte op, List<BlackViperModel> listWin10)
+        {
+            foreach (BlackViperModel item in listWin10)
+            {
+                string nameService = String.Empty;
+                if (item.Service_Name != null && item.Service_Name.Contains("_"))
+                {
+                    nameService = item.Service_Name.Split('_')[0];
+                }
+                else
+                {
+                    nameService = item.Service_Name;
+                }
+
+                if (item.DEFAULT_Windows_10_Home != null && item.DEFAULT_Windows_10_Home.Contains("Manual"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
+                }
+                else if (item.DEFAULT_Windows_10_Home != null && item.DEFAULT_Windows_10_Home.Contains("Automatic"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
+                }
+                else if (item.DEFAULT_Windows_10_Home != null && item.DEFAULT_Windows_10_Home.Contains("Disabled"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
+                }
+                else
+                {
+                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null" : nameService));
+                }
+            }
+            op = 0;
+        }
+
+        public void setDefault_WindowsPro(ref byte op, List<BlackViperModel> listWin10)
+        {
+            foreach (BlackViperModel item in listWin10)
+            {
+                string nameService = String.Empty;
+                if (item.Service_Name != null && item.Service_Name.Contains("_"))
+                {
+                    nameService = item.Service_Name.Split('_')[0];
+                }
+                else
+                {
+                    nameService = item.Service_Name;
+                }
+
+                if (item.DEFAULT_Windows_10_Pro != null && item.DEFAULT_Windows_10_Pro.Contains("Manual"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
+                }
+                else if (item.DEFAULT_Windows_10_Pro != null && item.DEFAULT_Windows_10_Pro.Contains("Automatic"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
+                }
+                else if (item.DEFAULT_Windows_10_Pro != null && item.DEFAULT_Windows_10_Pro.Contains("Disabled"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
+                }
+                else
+                {
+                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null" : nameService));
+                }
+            }
+            op = 0;
+        }
+
+        public void setBlackViper_SafeForDesktop(ref byte op, List<BlackViperModel> listWin10)
+        {
+            foreach (BlackViperModel item in listWin10)
+            {
+                string nameService = String.Empty;
+                if (item.Service_Name != null && item.Service_Name.Contains("_"))
+                {
+                    nameService = item.Service_Name.Split('_')[0];
+                }
+                else
+                {
+                    nameService = item.Service_Name;
+                }
+
+                if (item.Safe_Desktop != null && item.Safe_Desktop.Contains("Manual"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
+                }
+                else if (item.Safe_Desktop != null && item.Safe_Desktop.Contains("Automatic"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
+                }
+                else if (item.Safe_Desktop != null && item.Safe_Desktop.Contains("Disabled"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
+                }
+                else
+                {
+                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null" : nameService));
+                }
+            }
+            op = 0;
+        }
+
+        public void setBlackViper_SafeForLaptopOrTablet(ref byte op, List<BlackViperModel> listWin10)
+        {
+            foreach (BlackViperModel item in listWin10)
+            {
+                string nameService = String.Empty;
+                if (item.Service_Name != null && item.Service_Name.Contains("_"))
+                {
+                    nameService = item.Service_Name.Split('_')[0];
+                }
+                else
+                {
+                    nameService = item.Service_Name;
+                }
+
+                if (item.Safe_Desktop != null && item.Safe_Mobile.Contains("Manual"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
+                }
+                else if (item.Safe_Desktop != null && item.Safe_Mobile.Contains("Automatic"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
+                }
+                else if (item.Safe_Desktop != null && item.Safe_Mobile.Contains("Disabled"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
+                }
+                else
+                {
+                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null" : nameService));
+                }
+            }
+            op = 0;
+        }
+
+        public void setBlackViper_TweakedForDesktop(ref byte op, List<BlackViperModel> listWin10)
+        {
+            foreach (BlackViperModel item in listWin10)
+            {
+                string nameService = String.Empty;
+                if (item.Service_Name != null && item.Service_Name.Contains("_"))
+                {
+                    nameService = item.Service_Name.Split('_')[0];
+                }
+                else
+                {
+                    nameService = item.Service_Name;
+                }
+
+                if (item.Safe_Desktop != null && item.Tweaked_Desktop.Contains("Manual"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
+                }
+                else if (item.Safe_Desktop != null && item.Tweaked_Desktop.Contains("Automatic"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
+                }
+                else if (item.Safe_Desktop != null && item.Tweaked_Desktop.Contains("Disabled"))
+                {
+                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
+                }
+                else
+                {
+                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null" : nameService));
+                }
+            }
+            op = 0;
+        }
+    }
+}
--- ServiceOptimizer/Program.cs
+++ ServiceOptimizer/Program.cs
@@ -29,74 +29,84 @@ namespace ServiceOptimizer
         static void Main(string[] args)
         {
             byte op = 255;
+            byte so = 255;
             bool isByte = true;
-            if (runOnAdministratorMode())
-            {
-                CarregarJSON_BlackViper(windowsOrUserMode.windows);
+            bool json_isLoaded = false;
 
-                do
-                {
-                    Console.WriteLine($"Welcome to the service optimizer: \n");
-                    Console.WriteLine("1- Lists and Backup - Your Windows Services.");
-                    Console.WriteLine("2- BlackViper - Safe for Desktop.");
-                    Console.WriteLine("3- BlackViper - Safe for Laptop or Tablet.");
-                    Console.WriteLine("4- BlackViper - Tweaked for Desktop.");
-                    Console.WriteLine("5- Restore client Backup.");
-                    Console.WriteLine("0- Exit.");
-                    
-                    Console.Write("\nSelecione uma opção: ");
-                    Console.Out.Flush();
-                    isByte = Byte.TryParse(Console.ReadLine(), out op);
-                    //Convert.ToByte(Console.ReadLine());
-                    switch (op)
-                    {
-                        case 1:
-                            Console.Clear();
-                            ServicosWin.ListService();
-                            op = 255;
-                            break;
-                        case 2:
-                            Console.Clear();
-                            BlackViper_SafeForDesktop();
-                            Console.WriteLine($"{Environment.NewLine}{"".PadRight(105, '*')}");
-                            Console.WriteLine($"{Environment.NewLine} Processo Finalizado!");
-                            System.Threading.Thread.Sleep(3000);
-                            op = 0;
-                            break;
-                        case 3:
-                            Console.Clear();
-                            BlackViper_SafeForLaptopOrTablet();
-                            Console.WriteLine($"{Environment.NewLine}{"".PadRight(105, '*')}");
-                            Console.WriteLine($"{Environment.NewLine} Processo Finalizado!");
-                            System.Threading.Thread.Sleep(3000);
-                            op = 0;
-                            break;
-                        case 4:
-                            Console.Clear();
-                            BlackViper_TweakedForDesktop();
-                            Console.WriteLine($"{Environment.NewLine}{"".PadRight(105, '*')}");
-                            Console.WriteLine($"{Environment.NewLine} Processo Finalizado!");
-                            System.Threading.Thread.Sleep(3000);
-                            op = 0;
-                            break;
-                        case 5:
-                            Console.Clear();
-                            CarregarJSON_BlackViper(windowsOrUserMode.usuario);
-                            Restore_ClientBackup();
-                            Console.WriteLine($"{Environment.NewLine}{"".PadRight(105, '*')}");
-                            Console.WriteLine($"{Environment.NewLine} Processo Finalizado!");
-                            System.Threading.Thread.Sleep(3000);
-                            op = 0;
-                            break;
-                    }
-                } while (op != 0);
-            }
-            else
+            do
             {
-                //Console.WriteLine("Software não foi executado como administrador");
-                op = (byte)Console.Read();
-            }
-            
+                Console.Clear();
+                Console.WriteLine($"Welcome to the service optimizer - {Environment.OSVersion} {Environment.NewLine}");
+                Console.WriteLine($"Select the desired operating system: {Environment.NewLine}");
+                Console.WriteLine($"1 - Windows 10");
+                Console.WriteLine($"2 - Windows 8.1");
+                Console.WriteLine($"3 - Windows 8");
+                Console.WriteLine($"4 - Windows 7");
+                Console.WriteLine($"0 - Exit");
+
+                Console.Write($"{Environment.NewLine}Select an option: ");
+                Console.Out.Flush();
+                isByte = Byte.TryParse(Console.ReadLine(), out so);
+
+                Console.Clear();
+                Console.WriteLine($"Welcome to the service optimizer - {Environment.OSVersion} {Environment.NewLine}");
+
+                switch (so)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        do
+                        {
+                            Windows10 referencia = new Windows10(ref op);
+                            if (!json_isLoaded)
+                            {
+                                referencia.loadJSON(ref list);
+                                json_isLoaded = true;
+                            }
+
+                            switch (op)
+                            {
+                                case 1:
+                                    referencia.listAndBackup_WindowsServices(ref op);
+                                    break;
+                                case 2:
+                                    referencia.setBlackViper_SafeForDesktop(ref op, list);
+                                    break;
+                                case 3:
+                                    referencia.setBlackViper_SafeForLaptopOrTablet(ref op, list);
+                                    break;
+                                case 4:
+                                    referencia.setBlackViper_TweakedForDesktop(ref op, list);
+                                    break;
+                                case 5:
+                                    referencia.setDefault_WindowsHome(ref op, list);
+                                    break;
+                                case 6:
+                                    referencia.setDefault_WindowsPro(ref op, list);
+                                    break;
+                                case 7:
+                                    referencia.loadJSON(ref listUser);
+                                    referencia.Restore_WindowsServiceBackup(ref op, listUser);
+                                    break;
+                            }
+
+                            if(op != 255 && op != 0)
+                            {
+                                Console.WriteLine($"{Environment.NewLine}{"".PadRight(105, '*')}");
+                                Console.WriteLine($"{Environment.NewLine} Processo Finalizado!");
+                                System.Threading.Thread.Sleep(5000);
+                            }
+                        } while (op != 0);
+                        json_isLoaded = false;
+                        break;
+                    default:
+                        Console.WriteLine("Not Yet implemented, Sorry!!");
+                        System.Threading.Thread.Sleep(5000);
+                        break;
+                }
+
+            } while (so != 0);
         }
 
         public static bool runOnAdministratorMode()
@@ -145,161 +155,6 @@ namespace ServiceOptimizer
             catch(Exception ex)
             {
                 Console.WriteLine("Erro ao executar Leitura do Arquivo: {0}", ex.Message);
-            }
-        }
-
-        public static void CarregarJSON_BlackViper(windowsOrUserMode mode)
-        {
-            string sourceFile = string.Empty;
-            try
-            {
-                switch (mode)
-                {
-                    case windowsOrUserMode.windows:
-                        sourceFile = @"ConfigJSON.json";
-                        list = JsonConvert.DeserializeObject<List<BlackViperModel>>(File.ReadAllText(sourceFile));
-                        break;
-                    case windowsOrUserMode.usuario:
-                        sourceFile = @"ConfigUser.json";
-                        listUser = JsonConvert.DeserializeObject<List<ServiceClientModel>>(File.ReadAllText(sourceFile));
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Erro ao executar Leitura do Arquivo: {0}", ex.Message);
-            }
-        }
-
-        public static void BlackViper_SafeForDesktop()
-        {
-            foreach(BlackViperModel svcSafe in list)
-            {
-                string nameService = String.Empty;
-                if (svcSafe.Service_Name != null && svcSafe.Service_Name.Contains("_"))
-                {
-                    nameService = svcSafe.Service_Name.Split('_')[0];
-                }
-                else
-                {
-                    nameService = svcSafe.Service_Name;
-                }
-
-                if (svcSafe.Safe_Desktop != null && svcSafe.Safe_Desktop.Contains("Manual"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
-                }
-                else if (svcSafe.Safe_Desktop != null && svcSafe.Safe_Desktop.Contains("Automatic"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
-                }
-                else if (svcSafe.Safe_Desktop != null && svcSafe.Safe_Desktop.Contains("Disabled"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
-                }
-                else
-                {
-                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null" : nameService));
-                }
-            }
-        }
-
-        public static void BlackViper_SafeForLaptopOrTablet()
-        {
-            foreach (BlackViperModel svcSafe in list)
-            {
-                string nameService = String.Empty;
-                if (svcSafe.Service_Name != null && svcSafe.Service_Name.Contains("_"))
-                {
-                    nameService = svcSafe.Service_Name.Split('_')[0];
-                }
-                else
-                {
-                    nameService = svcSafe.Service_Name;
-                }
-
-                if (svcSafe.Safe_Mobile != null && svcSafe.Safe_Mobile.Contains("Manual"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
-                }
-                else if (svcSafe.Safe_Mobile != null && svcSafe.Safe_Mobile.Contains("Automatic"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
-                }
-                else if (svcSafe.Safe_Mobile != null && svcSafe.Safe_Mobile.Contains("Disabled"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
-                }
-                else
-                {
-                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null": nameService));
-                }
-            }
-        }
-
-        public static void BlackViper_TweakedForDesktop()
-        {
-            foreach (BlackViperModel svcSafe in list)
-            {
-                string nameService = String.Empty;
-                if (svcSafe.Service_Name != null && svcSafe.Service_Name.Contains("_"))
-                {
-                    nameService = svcSafe.Service_Name.Split('_')[0];
-                }
-                else
-                {
-                    nameService = svcSafe.Service_Name;
-                }
-
-                if (svcSafe.Tweaked_Desktop != null && svcSafe.Tweaked_Desktop.Contains("Manual"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
-                }
-                else if (svcSafe.Tweaked_Desktop != null && svcSafe.Tweaked_Desktop.Contains("Automatic"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
-                }
-                else if (svcSafe.Tweaked_Desktop != null && svcSafe.Tweaked_Desktop.Contains("Disabled"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
-                }
-                else
-                {
-                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", (String.IsNullOrEmpty(nameService) ? "Null" : nameService));
-                }
-            }
-        }
-
-        public static void Restore_ClientBackup()
-        {
-            foreach (ServiceClientModel svcSafe in listUser)
-            {
-                string nameService = String.Empty;
-                if (svcSafe.Service_Name.Contains("_"))
-                {
-                    nameService = svcSafe.Service_Name.Split('_')[0];
-                }
-                else
-                {
-                    nameService = svcSafe.Service_Name;
-                }
-
-                if (svcSafe.StartType.Contains("Manual"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Manual);
-                }
-                else if (svcSafe.StartType.Contains("Automatic"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Automatic);
-                }
-                else if (svcSafe.StartType.Contains("Disabled"))
-                {
-                    ServicosWin.ChangeStartMode(nameService, System.ServiceProcess.ServiceStartMode.Disabled);
-                }
-                else
-                {
-                    Console.WriteLine("SVC_NAME: {0} Não Configurado ou Instalado.", nameService);
-                }
             }
         }
     }
